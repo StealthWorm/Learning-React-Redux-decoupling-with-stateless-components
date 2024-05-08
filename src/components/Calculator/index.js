23647700dae7ae54b9111d31aac0ac8582b12011
StealthWorm <thierrypitela@hotmail.com>
2024-05-07 21:22:15 -0300

    feat: decoupling react view from redux store + creting redux store actions management

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
+import './Calculator.css';
+
 import Display from '../Display';
 import ControlPanel from '../ControlPanel';
 import Digits from '../Digits';
 import Operators from '../Operators';
 import History from '../History';
-import './Calculator.css';
-import ee from '../../eventEmitter';
+
 import store from '../../store';
 
 class Calculator extends Component {
-  constructor(props) {
-    super(props);
-
-    ee.addListener('displayUpdate', () => this.forceUpdate()); // coming from React.Component class
-  }
   render() {
     return (
       <main className="react-calculator">
-        <Display text={store.curExpression} />
+        <Display text={store.getState().curExpression} />
         <ControlPanel />
         <Digits />
         <Operators />
-        <History />
+        <History
+          showHistory={store.getState().showHistory}
+          history={store.getState().history}
+        />
       </main>
     )
   }
--- src/components/ControlPanel/index.js
+++ src/components/ControlPanel/index.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import ee from '../../eventEmitter';
 import store from '../../store';
 import Button from '../Button';
+import { updateExpression } from '../../actions/expression';
+import { toggleHistory } from '../../actions/history';
 
-export const showHistory = () => {
-    ee.emitEvent('toggle-history');
-}
+export const showHistory = () => toggleHistory();
 
-export const clearDisplay = () => {
-    store.newExpression = 0;
-}
+export const clearDisplay = () => updateExpression(0);
 
 export const removeOneChar = () => {
-    const curExpression = String(store.curExpression);
+    const curExpression = String(store.getState().curExpression);
     const newExpWithRemovedChar = curExpression.toString().trim().substring(0, (curExpression.length - 1));
 
-    return store.newExpression = newExpWithRemovedChar === '' ? 0 : newExpWithRemovedChar;
+    return updateExpression(newExpWithRemovedChar === '' ? 0 : newExpWithRemovedChar);
 }
 
 export const ControlPanel = () => (
